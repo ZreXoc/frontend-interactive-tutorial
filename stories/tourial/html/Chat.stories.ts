1cf28e6bcfb0898e0eac9bb764d80a79ffd0a714
ZreXoc <zeexoc@outlook.com>
2025-05-25 10:56:32 +0800

    fix: page title and link

--- stories/tourial/html/Chat.stories.ts
+++ stories/tourial/html/Chat.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/html';
 import { createChatElement } from './Chat';
 
 const meta = {
-  title: '前端入门交互式教程/01.HTML基础/03.页面结构',
+  title: '前端入门交互式教程/01.HTML基础/03.聊天室示例',
   render: () => createChatElement(),
 } satisfies Meta;
 
